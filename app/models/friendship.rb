8cc1d76302bd7c01d2086e526d7e9049ceaf9b6b
Bhatiya Jayasuriya <bhatiyajayasuriya@gmail.com>
2017-04-10 22:38:29 +0000

    add friendship views

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,6 @@
 class Friendship < ActiveRecord::Base
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+  # 'class_name' is the keyword we need to use for self referential association
+  # use keyword 'class' in the migration file
 end
